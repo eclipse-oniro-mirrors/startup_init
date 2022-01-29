8fffc0b7efc92f36a6233f7203addef8a94b4d8f
yudechen <chenyude@huawei.com>
2022-01-29 11:19:24 +0800

    添加查询设备是否支持指定的SystemCapability。 【背景】
    【修改方案】
    添加功能接口。
    
    【影响】
    
    Change-Id: Ic7ef6046e33fec7baf0de87cd29b2643b946991a
    Signed-off-by: yudechen <chenyude@huawei.com>

---
+++ interfaces/innerkits/syscap/init_syscap.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "sys_param.h"
+#include "beget_ext.h"
+#include "securec.h"
+#include "systemcapability.h"
+
+#define SYSCAP_MAX_SIZE 100
+#define SYSCAP_PREFIX_NAME "SystemCapability"
+
+bool HasSystemCapability(const char *cap)
+{
+    char capName[SYSCAP_MAX_SIZE] = { 0 };
+    char paramValue[PARAM_VALUE_LEN_MAX] = { 0 };
+    unsigned int valueLen = PARAM_VALUE_LEN_MAX;
+
+    if (strncmp(SYSCAP_PREFIX_NAME, cap, sizeof(SYSCAP_PREFIX_NAME) - 1) != 0) {
+        if (strncpy_s(capName, sizeof(capName), cap, sizeof(capName) - 1) < 0) {
+            BEGET_LOGE("Failed strncpy_s err=%d", errno);
+            return false;
+        }
+    } else if (snprintf_s(capName, SYSCAP_MAX_SIZE, SYSCAP_MAX_SIZE - 1, SYSCAP_PREFIX_NAME".%s", cap) == -1) {
+        BEGET_LOGE("Failed snprintf_s err=%d", errno);
+        return false;
+    }
+
+    if (SystemGetParameter(capName, paramValue, &valueLen) != 0) {
+        BEGET_LOGE("Failed get paramName.");
+        return false;
+    }
+    
+    return true;
+}
