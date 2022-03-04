6f509c922c97ceefc768a11ad14958f79cfe9d2e
xionglei6 <xionglei6@huawei.com>
2022-03-04 12:43:26 +0800

    fix：支持suspend
    Signed-off-by: xionglei6 <xionglei6@huawei.com>

--- services/param/trigger/trigger_processor.c
+++ services/param/trigger/trigger_processor.c
@@ -228,7 +228,7 @@ static int GetTriggerType(const char *type)
     }
     const char *triggerTypeStr[] = {
         "pre-init", "boot", "early-init", "init", "early-init", "late-init", "post-init",
-        "fs", "early-fs", "post-fs", "late-fs", "early-boot", "post-fs-data", "reboot"
+        "fs", "early-fs", "post-fs", "late-fs", "early-boot", "post-fs-data", "reboot", "suspend"
     };
     for (size_t i = 0; i < ARRAY_LENGTH(triggerTypeStr); i++) {
         if (strcmp(triggerTypeStr[i], type) == 0) {
