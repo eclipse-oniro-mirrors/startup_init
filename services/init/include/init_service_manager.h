a8c1dc959a01a516dba5f1ff51b44ac5c34c87c2
cheng_jinsong <chengjinsong2@huawei.com>
2022-08-23 16:33:23 +0800

    fix bug for kill appspawn
    Signed-off-by: cheng_jinsong <chengjinsong2@huawei.com>

--- services/init/include/init_service_manager.h
+++ services/init/include/init_service_manager.h
@@ -63,6 +63,7 @@ void LoadAccessTokenId(void);
 Service *AddService(const char *name);
 void DumpServiceHookExecute(const char *name, const char *info);
 void ProcessControlFd(uint16_t type, const char *serviceCmd, const void *context);
+int GetKillServiceSig(const char *name);
 #ifdef __cplusplus
 #if __cplusplus
 }
