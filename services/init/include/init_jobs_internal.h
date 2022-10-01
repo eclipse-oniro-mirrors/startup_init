ee12335d00d9115f53baaa1a87625881f52b7269
yangziyong <nsyangziyong@huawei.com>
2022-10-01 09:38:39 +0800

    fix: using light interface control led instead of file io
    Signed-off-by: yangziyong <nsyangziyong@huawei.com>

--- services/init/include/init_jobs_internal.h
+++ services/init/include/init_jobs_internal.h
@@ -44,7 +44,7 @@ int DoJobNow(const char *jobName);
 #define INIT_CONFIGURATION_FILE "/etc/init.without_two_stages.cfg"
 #endif
 #define OTHER_CFG_PATH "/system/etc/init"
-#define OTHER_CHARGE_PATH "/system/etc/charge"
+#define OTHER_CHARGE_PATH "/vendor/etc/charger"
 #define MAX_PATH_ARGS_CNT 20
 
 void ReadConfig(void);
