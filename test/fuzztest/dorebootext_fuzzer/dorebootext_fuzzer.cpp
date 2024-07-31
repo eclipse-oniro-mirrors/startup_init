c1060dc297df61da91ec3588f68b56553f24e18e
陈其文 <chenqiwen6@huawei.com>
2024-07-31 17:22:49 +0800

    fuzztest代码告警修改
    Signed-off-by: 陈其文 <chenqiwen6@huawei.com>

--- test/fuzztest/dorebootext_fuzzer/dorebootext_fuzzer.cpp
+++ test/fuzztest/dorebootext_fuzzer/dorebootext_fuzzer.cpp
@@ -14,7 +14,7 @@
  */
 
 #include "dorebootext_fuzzer.h"
-#include<string>
+#include <string>
 #include "init_reboot.h"
 
 namespace OHOS {
--- test/fuzztest/getparametercpp_fuzzer/getparametercpp_fuzzer.cpp
+++ test/fuzztest/getparametercpp_fuzzer/getparametercpp_fuzzer.cpp
@@ -14,7 +14,7 @@
  */
 
 #include "getparametercpp_fuzzer.h"
-#include<string>
+#include <string>
 #include "parameters.h"
 
 namespace OHOS {
