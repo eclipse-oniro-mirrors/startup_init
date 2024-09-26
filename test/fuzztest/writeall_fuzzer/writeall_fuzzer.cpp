8f429aabd1155308458634ad9bf15addae529917
lwx1281857 <linnanmu@h-partners.com>
2024-09-26 15:20:17 +0800

    Description:add FuzzTest Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: lwx1281857 <linnanmu@h-partners.com>

---
+++ test/fuzztest/writeall_fuzzer/writeall_fuzzer.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "init_utils.h"
+#include <string>
+#include "writeall_fuzzer.h"
+
+namespace OHOS {
+    bool FuzzGetParameterFromCmdLine(const uint8_t* data, size_t size)
+    {
+        CheckAndCreateDir("/data/init_fuzztest/FuzzTestData");
+        int fd = open("/data/init_fuzztest/FuzzTestData", O_CREAT | O_RDWR, 0777);
+        if (fd < 0) {
+            return false;
+        }
+        std::string str(reinterpret_cast<const char*>(data), size);
+        size_t ret = WriteAll(fd, str.c_str(), size);
+        if (ret == 0) {
+            close(fd);
+            return false;
+        };
+        close(fd);
+        return true;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::FuzzGetParameterFromCmdLine(data, size);
+    return 0;
+}
