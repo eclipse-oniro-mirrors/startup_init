d1d499f23b325ed68e3016208944be84e48fb0fa
张珂 <zhangke172@huawei.com>
2025-03-24 12:58:43 +0000

    update interfaces/innerkits/syspara/parameter.c.
    Signed-off-by: 张珂 <zhangke172@huawei.com>

--- interfaces/innerkits/syspara/parameter.c
+++ interfaces/innerkits/syspara/parameter.c
@@ -427,11 +427,11 @@ int GetPerformanceClass(void)
     static const char *performanceClass = NULL;
     GetProperty("const.sys.performance_class", &performanceClass);
     if (performanceClass == NULL) {
-        return PERFORMANCE_CLASS_MIN_LEVEL;
+        return PERFORMANCE_CLASS_HIGH_LEVEL;
     }
     int performanceClassValue = atoi(performanceClass);
-    if (performanceClassValue < PERFORMANCE_CLASS_MIN_LEVEL || performanceClassValue > PERFORMANCE_CLASS_MAX_LEVEL) {
-        return PERFORMANCE_CLASS_MIN_LEVEL;
+    if (performanceClassValue < PERFORMANCE_CLASS_HIGH_LEVEL || performanceClassValue > PERFORMANCE_CLASS_LOW_LEVEL) {
+        return PERFORMANCE_CLASS_HIGH_LEVEL;
     }
     return performanceClassValue;
 }
