0aea5ede7fc0c78d32acc0e3583d28f2741a6f5b
yuyanqinghw <yuyanqing539@huawei.com>
2025-04-15 21:54:29 +0800

    startup for AB patch partition
    Signed-off-by: yuyanqinghw <yuyanqing539@huawei.com>

--- ueventd/ueventd.c
+++ ueventd/ueventd.c
@@ -189,7 +189,8 @@ static void HandleRequiredBlockDeviceNodes(const struct Uevent *uevent, char **d
             strstr(uevent->partitionName, "rvt") != NULL ||
             strstr(uevent->partitionName, "dtbo") != NULL ||
             strstr(uevent->partitionName, "modem") != NULL ||
-            strstr(uevent->partitionName, "patch") != NULL) {
+            strcmp(uevent->partitionName, "patch_a") == 0 ||
+            strcmp(uevent->partitionName, "patch_b") == 0) {
             INIT_LOGI("Handle required partitionName %s", uevent->partitionName);
             HandleBlockDeviceEvent(uevent);
             return;
