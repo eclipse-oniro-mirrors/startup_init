403d49545df566e8ac98bb4d4aa7d1e571926b49
lwx1281857 <linnanmu@h-partners.com>
2025-04-19 10:20:43 +0800

    Description: add autotest Feature or Bugfix: Bugfix Binary Source:No
    Signed-off-by: lwx1281857 <linnanmu@h-partners.com>

---
+++ test/autotest/testcases/SUB_STARTUP_INIT_SHELLMGT/SUB_STARTUP_INIT_SHELLMGT_0700.py
@@ -0,0 +1,47 @@
+#
+# Copyright (c) 2025 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# -*- coding: utf-8 -*-
+from devicetest.core.test_case import Step, TestCase
+from hypium import UiDriver
+
+
+class SubStartupInitShellmgt0700(TestCase):
+
+    def __init__(self, controllers):
+        self.tag = self.__class__.__name__
+        self.tests = [
+            "test_step1"
+        ]
+        TestCase.__init__(self, self.tag, controllers)
+        self.driver = UiDriver(self.device1)
+
+    def setup(self):
+        Step("预置工作:初始化手机开始.................")
+        Step(self.devices[0].device_id)
+
+    def test_step1(self):
+        Step("查看条件..........................")
+        condition = self.driver.shell('cat /etc/init.cfg|grep chown')
+        Step(condition)
+        Step("条件包括/dev/access_token_id..........................")
+        self.driver.Assert.contains(condition, 'chown access_token access_token /dev/access_token_id')
+        Step("校验条件是否生效..........................")
+        condition = self.driver.shell('ls -l /dev/access_token_id')
+        Step(condition)
+        self.driver.Assert.contains(condition, 'access_token access_token')
+
+    def teardown(self):
+        Step("收尾工作.................")
