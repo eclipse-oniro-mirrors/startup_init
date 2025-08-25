4ee3b7c2f7e277d28541c3ba5e967054d38fe015
宋佳伟 <songjiawei9@h-partners.com>
2025-08-25 14:54:59 +0800

    Description:add autotest testcases IssueNo:https://gitee.com/openharmony/startup_init/issues/ICUJK2 Binary Source:No Signed-off-by: songjiawei9 <songjiawei9@h-partners.com>

---
+++ test/autotest/testcases/sub_startup_init_basicenv/sub_startup_init_basicenv_0200.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from devicetest.core.test_case import Step, TestCase
+from hypium import UiDriver
+
+
+class SubStartupInitBasicenv0200(TestCase):
+
+    def __init__(self, controllers):
+        self.tag = self.__class__.__name__
+        self.tests = [
+            "test_step1"
+        ]
+        TestCase.__init__(self, self.tag, controllers)
+        self.driver = UiDriver(self.device1)
+
+    def setup(self):
+        Step("预置工作:初始化手机开始.................")
+        Step(self.devices[0].device_id)
+
+    def test_step1(self):
+        Step("find /dev/unix/socket................")
+        result_socket = self.driver.shell("ls -l /dev/unix/socket")
+        Step(result_socket)
+        self.driver.Assert.contains(result_socket, 'hdcd', "ERROR: hdcd is not exist in socket directory.")
+        self.driver.Assert.contains(result_socket, 'hilogInput', "ERROR: hilogInput is not exist in socket directory.")
+        self.driver.Assert.contains(result_socket, 'hilogControl', "ERROR: hilogControl is not exist in socket directory.")
+        self.driver.Assert.contains(result_socket, 'hisysevent', "ERROR: hisysevent is not exist in socket directory.")
+
+    def teardown(self):
+        Step("收尾工作.................")
