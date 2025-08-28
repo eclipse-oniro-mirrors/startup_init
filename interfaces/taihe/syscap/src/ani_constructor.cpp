1465fef9df8f018c4c55a754a32bedb5358a6740
cat <chenjinxiang3@huawei.com>
2025-08-28 10:37:39 +0800

    repair caniuse code
    Signed-off-by: cat <chenjinxiang3@huawei.com>

--- interfaces/taihe/syscap/src/ani_constructor.cpp
+++ interfaces/taihe/syscap/src/ani_constructor.cpp
@@ -12,16 +12,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "taihe/runtime.hpp"
-#include "global.ani.hpp"
+#include "taihe.platform.ani.ani.hpp"
+#include "internal.full.global.ani.hpp"
+#if __has_include(<ani.h>)
+#include <ani.h>
+#elif __has_include(<ani/ani.h>)
+#include <ani/ani.h>
+#else
+#error "ani.h not found, Please ensure the Ani SDK is correctly installed."
+#endif
 ANI_EXPORT ani_status ANI_Constructor(ani_vm *vm, uint32_t *result)
 {
     ani_env *env;
     if (ANI_OK != vm->GetEnv(ANI_VERSION_1, &env)) {
         return ANI_ERROR;
     }
-    if (ANI_OK != global::ANIRegister(env)) {
-        std::cerr << "Error from global::ANIRegister" << std::endl;
+    if (ANI_OK != internal::full::global::ANIRegister(env)) {
+        std::cerr << "Error from internal::full::global::ANIRegister" << std::endl;
         return ANI_ERROR;
     }
     *result = ANI_VERSION_1;
