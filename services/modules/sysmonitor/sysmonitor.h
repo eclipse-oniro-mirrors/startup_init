e320a92008cf12c50652c4a2709b8bbdb2be2a5b
d60129457 <duyiheng@h-partners.com>
2026-08-10 11:43:45 +0800

    update: 更新文件 sysmonitor.h
    Signed-off-by: d60129457 <duyiheng@h-partners.com>

--- services/modules/sysmonitor/sysmonitor.h
+++ services/modules/sysmonitor/sysmonitor.h
@@ -16,9 +16,9 @@
 #ifndef STARTUP_INIT_SYSMONITOR_H
 #define STARTUP_INIT_SYSMONITOR_H
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <time.h>
-#include <stdbool.h>
 #include "list.h"
 
 #ifdef __cplusplus
@@ -54,18 +54,6 @@ extern "C" {
 #define NSEC_PER_MSEC               1000000LL
 #define NSEC_PER_USEC               1000LL
 
-#define CPU_CRITICAL_THRESHOLD      9000
-#define CPU_WARNING_THRESHOLD       8000
-#define MEM_CRITICAL_THRESHOLD      9000
-#define MEM_WARNING_THRESHOLD       8500
-#define SWAP_USAGE_INFO_THRESHOLD   5000
-#define BLOCKED_PROC_THRESHOLD      100
-#define STAT_FIELD_MAX_COUNT        50
-#define PROC_PATH_LEN               256
-#define CPU_LABEL_LEN               16
-#define MEM_KEY_LEN                 64
-#define MEM_UNIT_LEN                16
-
 #define PROC_STAT_PATH              "/proc/stat"
 #define PROC_MEMINFO_PATH           "/proc/meminfo"
 #define PROC_DISKSTATS_PATH         "/proc/diskstats"
@@ -76,19 +64,16 @@ extern "C" {
 #define PROC_DIR_PATH               "/proc"
 
 #define CPU_LABEL_PREFIX            "cpu"
-#define CTXT_LINE_PREFIX            "ctxt "
-#define PROCESSES_LINE_PREFIX       "processes "
-#define PROCS_RUNNING_PREFIX        "procs_running "
-#define PROCS_BLOCKED_PREFIX        "procs_blocked "
-
-#define PROCESS_NAME_MAX_LEN        64
-#define PROCESS_CMDLINE_MAX_LEN     256
+#define CPU_LABEL_LEN               16
+#define MEM_KEY_LEN                 64
+#define MEM_UNIT_LEN                16
 #define DEVICE_NAME_MAX_LEN         64
 #define NET_IFACE_MAX_LEN           32
 #define ALARM_MSG_MAX_LEN           256
 #define PERF_NAME_MAX_LEN           64
 #define FS_TYPE_MAX_LEN             64
 #define MOUNT_POINT_MAX_LEN         256
+#define PROCESS_NAME_MAX_LEN        64
 
 typedef enum {
     MONITOR_TYPE_CPU = 0,
@@ -140,35 +125,8 @@ typedef struct {
     uint64_t availableMem;
     uint64_t buffers;
     uint64_t cached;
-    uint64_t swapCached;
-    uint64_t activeMem;
-    uint64_t inactiveMem;
-    uint64_t activeAnon;
-    uint64_t inactiveAnon;
-    uint64_t activeFile;
-    uint64_t inactiveFile;
-    uint64_t unevictable;
-    uint64_t mlocked;
     uint64_t swapTotal;
     uint64_t swapFree;
-    uint64_t dirtyPages;
-    uint64_t writeback;
-    uint64_t anonPages;
-    uint64_t mapped;
-    uint64_t shmem;
-    uint64_t slab;
-    uint64_t slabReclaimable;
-    uint64_t slabUnreclaimable;
-    uint64_t kernelStack;
-    uint64_t pageTables;
-    uint64_t nfsUnstable;
-    uint64_t bounce;
-    uint64_t writebackTmp;
-    uint64_t commitLimit;
-    uint64_t committedAs;
-    uint64_t vmallocTotal;
-    uint64_t vmallocUsed;
-    uint64_t vmallocChunk;
     uint32_t usagePercent;
     uint32_t swapUsagePercent;
 } MemoryStats;
@@ -176,37 +134,15 @@ typedef struct {
 typedef struct ProcessInfo {
     ListNode node;
     int32_t pid;
-    int32_t ppid;
     char name[PROCESS_NAME_MAX_LEN];
-    char state;
-    uint64_t utime;
-    uint64_t stime;
-    int64_t priority;
-    int64_t nice;
-    int64_t numThreads;
-    int64_t vsize;
-    int64_t rss;
-    uint64_t startTime;
-    uint64_t delayBlkIo;
     uint64_t cpuUsage;
     uint64_t memUsage;
-    char cmdline[PROCESS_CMDLINE_MAX_LEN];
-    uint32_t flags;
 } ProcessInfo;
 
 typedef struct {
     char deviceName[DEVICE_NAME_MAX_LEN];
     uint64_t readsCompleted;
-    uint64_t readsMerged;
-    uint64_t sectorsRead;
-    uint64_t readTimeMs;
     uint64_t writesCompleted;
-    uint64_t writesMerged;
-    uint64_t sectorsWritten;
-    uint64_t writeTimeMs;
-    uint64_t ioInProgress;
-    uint64_t ioTimeMs;
-    uint64_t weightedIoTimeMs;
     uint64_t readBytesPerSec;
     uint64_t writeBytesPerSec;
     uint32_t utilization;
@@ -215,21 +151,7 @@ typedef struct {
 typedef struct {
     char interface[NET_IFACE_MAX_LEN];
     uint64_t rxBytes;
-    uint64_t rxPackets;
-    uint64_t rxErrors;
-    uint64_t rxDropped;
-    uint64_t rxFifo;
-    uint64_t rxFrame;
-    uint64_t rxCompressed;
-    uint64_t rxMulticast;
     uint64_t txBytes;
-    uint64_t txPackets;
-    uint64_t txErrors;
-    uint64_t txDropped;
-    uint64_t txFifo;
-    uint64_t txCollisions;
-    uint64_t txCarrier;
-    uint64_t txCompressed;
     uint64_t rxBytesPerSec;
     uint64_t txBytesPerSec;
 } NetworkStats;
@@ -284,8 +206,6 @@ typedef struct {
     uint32_t recordCount;
 } MonitorContext;
 
-typedef void (*MonitorCallback)(MonitorType type, const void *data, void *context);
-
 int InitMonitor(const MonitorConfig *config);
 void DestroyMonitor(void);
 int StartMonitor(void);
@@ -314,22 +234,13 @@ PerfRecord *BeginPerfRecord(const char *name, uint32_t type);
 int EndPerfRecord(PerfRecord *record);
 const ListNode *GetPerfRecords(void);
 
-void PrintMonitorSummary(void);
-int ExportMonitorData(const char *filePath);
-
 uint32_t CalculateCpuUsage(const CpuStats *prev, const CpuStats *curr);
 uint32_t CalculateMemUsage(const MemoryStats *stats);
 
 ProcessInfo *FindTopCpuProcess(void);
 ProcessInfo *FindTopMemProcess(void);
 
-int RunSystemDiagnosis(const char *outputPath);
-
-int RegisterMonitorCallback(MonitorType type, MonitorCallback callback, void *context);
-int UnregisterMonitorCallback(MonitorType type);
-
 int CheckThresholds(void);
-int GetHistoryStats(MonitorType type, void *stats, uint32_t index);
 
 #ifdef __cplusplus
 #if __cplusplus
