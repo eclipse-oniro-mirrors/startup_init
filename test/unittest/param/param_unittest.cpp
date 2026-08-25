76c4ceb39deaa11815a20d5304542e6e86a1a8a4
wangjianru <wangjianru4@h-partners.com>
2026-08-25 16:46:22 +0800

    update: 更新文件 param_unittest.cpp
    Signed-off-by: wangjianru <wangjianru4@h-partners.com>

--- test/unittest/param/param_unittest.cpp
+++ test/unittest/param/param_unittest.cpp
@@ -724,7 +724,7 @@ HWTEST_F(ParamUnitTest, Init_TestParamCache_001, TestSize.Level0)
     CachedParameterDestroy(cacheHandle3);
 }
 
-HWTEST_F(ParamUnitTest, Init_TestUpdataPersistCommitIdNullArea_001, TestSize.Level0)
+HWTEST_F(ParamUnitTest, Init_TestUpdatePersistCommitIdNullArea_001, TestSize.Level0)
 {
     ParamUnitTest test;
     WorkSpace *space = GetWorkSpace(WORKSPACE_INDEX_DAC);
@@ -732,15 +732,14 @@ HWTEST_F(ParamUnitTest, Init_TestUpdataPersistCommitIdNullArea_001, TestSize.Lev
     ASSERT_NE(space->area, nullptr);
     ParamTrieHeader *savedArea = space->area;
     space->area = nullptr;
-
-    WritePersistParam("persist.test.nullarea.check", "tesy_value");
+    WritePersistParam("persist.test.nullarea.check", "test_value");
     EXPECT_EQ(space->area, nullptr);
     space->area = savedArea;
 
     ParamWorkSpace *paramSpace = GetParamWorkSpace();
     ASSERT_NE(paramSpace, nullptr);
     WorkSpace *savedWorkSpace = paramSpace->workSpace[WORKSPACE_INDEX_DAC];
-    paramSpacee->workSpace[WORKSPACE_INDEX_DAC] = nullptr;
+    paramSpace->workSpace[WORKSPACE_INDEX_DAC] = nullptr;
     int ret = WritePersistParam("persist.test.nullarea.check2", "test_value2");
     EXPECT_EQ(ret, 0);
     EXPECT_EQ(paramSpace->workSpace[WORKSPACE_INDEX_DAC], nullptr);
